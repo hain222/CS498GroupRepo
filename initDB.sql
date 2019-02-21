c0159c2c00e832d28188ea85dafa6ec02aed939c
Harrison Inocencio <hain222@uky.edu>
2019-02-21 15:06:43 -0500

    Add load commands and fix database table declarations

--- initDB.sql
+++ initDB.sql
@@ -14,43 +14,57 @@ CREATE TABLE User (
 );
 
 CREATE TABLE Song (
-	songId INT NOT NULL PRIMARY KEY,
+	songId VARCHAR(22) NOT NULL PRIMARY KEY,
 	name VARCHAR(64) NOT NULL,
 	relDate VARCHAR(32)
 );
 
 CREATE TABLE Artist (
-	artistId INT NOT NULL PRIMARY KEY,
+	artistId VARCHAR(22) NOT NULL PRIMARY KEY,
 	name VARCHAR(64) NOT NULL
 );
 
 CREATE TABLE Album (
-	albumId INT NOT NULL PRIMARY KEY,
+	albumId VARCHAR(22) NOT NULL PRIMARY KEY,
+	albumImg VARCHAR(128),
 	name VARCHAR(64) NOT NULL,
 	relDate VARCHAR(32),
 	numTracks INT
 );
 
 CREATE TABLE SongAlbum (
-	songId INT NOT NULL,
-	albumId INT NOT NULL,
+	albumId VARCHAR(22) NOT NULL,
+	songId VARCHAR(22) NOT NULL,
 	PRIMARY KEY (songId, albumId),
 	FOREIGN KEY(songId) REFERENCES Song(songId),
 	FOREIGN KEY(albumId) REFERENCES Album(albumId)
 );
 
 CREATE TABLE SongArtist (
-	songId INT NOT NULL,
-	artistId INT NOT NULL,
+	artistId VARCHAR(22) NOT NULL,
+	songId VARCHAR(22) NOT NULL,
 	PRIMARY KEY (songId, artistId),
 	FOREIGN KEY(songId) REFERENCES Song(songId),
 	FOREIGN KEY(artistId) REFERENCES Artist(artistId)
 );
 
 CREATE TABLE AlbumArtist (
-	albumId INT NOT NULL,
-	artistId INT NOT NULL,
+	albumId VARCHAR(22) NOT NULL,
+	artistId VARCHAR(22) NOT NULL,
 	PRIMARY KEY (albumId, artistId),
 	FOREIGN KEY(albumId) REFERENCES Album(albumId),
 	FOREIGN KEY(artistId) REFERENCES Artist(artistId)
 );
+
+LOAD DATA LOCAL INFILE "/home/hain222/cs498/proj/CS498GroupRepo/jupyter/songs.dat" INTO TABLE Song 
+	FIELDS TERMINATED BY '|';
+LOAD DATA LOCAL INFILE "/home/hain222/cs498/proj/CS498GroupRepo/jupyter/albums.dat" INTO TABLE Album 
+	FIELDS TERMINATED BY '|';
+LOAD DATA LOCAL INFILE "/home/hain222/cs498/proj/CS498GroupRepo/jupyter/artists.dat" INTO TABLE Artist 
+	FIELDS TERMINATED BY '|';
+LOAD DATA LOCAL INFILE "/home/hain222/cs498/proj/CS498GroupRepo/jupyter/song-album.dat" INTO TABLE SongAlbum 
+	FIELDS TERMINATED BY '|';
+LOAD DATA LOCAL INFILE "/home/hain222/cs498/proj/CS498GroupRepo/jupyter/song-artist.dat" INTO TABLE SongArtist 
+	FIELDS TERMINATED BY '|';
+LOAD DATA LOCAL INFILE "/home/hain222/cs498/proj/CS498GroupRepo/jupyter/album-artist.dat" INTO TABLE AlbumArtist
+	FIELDS TERMINATED BY '|';
