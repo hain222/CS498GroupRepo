f3aa0d0ce5712958600c826ccda719c7adb71260
Jordan George <jordanngeorge@gmail.com>
2019-03-18 18:29:29 -0400

    show data on web app

--- test/awesomeapp/songs/views.py
+++ test/awesomeapp/songs/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Song
 
 # Create your views here.
+
+
+def home(request):
+    songs = Song.objects.all()
+
+    return render(
+        request,
+        'songs/home.html',
+        {
+            'songs': songs
+        }
+    )
