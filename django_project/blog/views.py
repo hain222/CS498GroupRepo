cf8d05d4f79ce82fd528cd91c0977da816a82d24
Jordan George <jordanngeorge@gmail.com>
2019-03-18 20:17:34 -0400

    add song data to home page

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,14 +1,39 @@
 from django.shortcuts import render
-from .models import Song
+from .models import Song, Songartist, Artist, Album, Songalbum
 
 
 def home(request):
 	songs = Song.objects.all()
+	artists = Artist.objects.all()
+	albums = Album.objects.all()
+
+	songids = Song.objects.values('songid')
+	# artistids = Songartist.objects.filter(songid__in=songids).values('artistid')
+	# artists = Artist.objects.filter(artistid__in=artistids)
+
+	songartistdict = dict()
+	for i in Songartist.objects.filter(songid__in=songids).values('artistid', 'songid'):
+		if i['songid'] in songartistdict:
+			# append the new number to the existing array at this slot
+			songartistdict[i['songid']].append(i['artistid'])
+		else:
+			# create a new array in this slot
+			songartistdict[i['songid']] = [i['artistid']]
+
+	songalbumdict = dict()
+	for i in Songalbum.objects.filter(songid__in=songids).values('albumid', 'songid'):
+		# print(i)
+		songalbumdict[i['songid']] = i['albumid']
+
 
 	context = {
-		'songs': songs
-
+		'songs': songs,
+		'artists': artists,
+		'albums': albums,
+		'songartistdict': songartistdict,
+		'songalbumdict': songalbumdict
 	}
+
 	return render(request, 'blog/home.html', context)
 
 def about(request):
--- test/awesomeappenv/lib/python3.6/types.py
+++
@@ -1,1 +0,0 @@
-/Users/jordangeorge/anaconda3/lib/python3.6/types.py
